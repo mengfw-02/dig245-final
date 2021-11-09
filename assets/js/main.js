6cd1a41e76c4b766ebdb927c0604be9bfc907dfd
Meng Fan Wang <mengfanwang724@gmail.com>
2021-11-08 19:11:09 -0500

    draft #1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,21 @@
-
 /* javascript */
+$(document).ready(function() {
+  var input;
+  $("form").on('submit',function(event) {
+    input = $( "habitInput" ).first().val();
+    console.log(input);
+    event.preventDefault();
+  });
+  // $("submit_habit").click(function() {
+  //   var input = document.getElementById("habitInput");
+  //   console.log(input);
+  // });
+  // $.ajax({
+  //   type: "POST",
+  //   data: input,
+  //   sucess: function(){
+  //     console.log("sucess");
+  //   }
+  // });
+
+});
