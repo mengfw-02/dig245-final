ea04b97b0553f2a92c9b0ce870640316a5c301ac
mengfw-02 <63938598+mengfw-02@users.noreply.github.com>
2021-11-29 12:10:34 -0500

    draft

--- assets/js/new.js
+++ assets/js/new.js
@@ -1,10 +1,5 @@
 var pages ={
-  "about":{
-    "title": "About Habiter"
-    "content": "Hi, I'm Habiter: a bot that helps you keep track of your good/bad habit."
-  },
-  "habiter":{
-    "title": "Today's Date: xx/xx/xxxx"
-    "content": ""
-  }
+  "habiter": "empty";
+  "days": 0;
 }
+$("#main")
