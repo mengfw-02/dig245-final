881961bfc640f736c570dcc9507bbd2636da40f3
mengfw-02 <63938598+mengfw-02@users.noreply.github.com>
2021-11-28 23:01:53 -0500

    updated

---
+++ assets/js/new.js
@@ -0,0 +1,10 @@
+var pages ={
+  "about":{
+    "title": "About Habiter"
+    "content": "Hi, I'm Habiter: a bot that helps you keep track of your good/bad habit."
+  },
+  "habiter":{
+    "title": "Today's Date: xx/xx/xxxx"
+    "content": ""
+  }
+}
