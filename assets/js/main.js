d4f6013aa5fe7f44559b44e057ff48ad71bb6a0d
Meng Fan Wang <mengfanwang724@gmail.com>
2021-12-06 22:38:47 -0500

    draft #6

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,103 +1,111 @@
 /* javascript */
 $(document).ready(function() {
-  function setCookie(cname,cvalue,exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires=" + d.toGMTString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
 
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  console.log(decodedCookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+  function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    console.log(decodedCookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
+    return "";
+  }
+  $("#toBuild").hide();
+  $("form").on('submit', function(event) {
+    event.preventDefault();
+    let val = document.getElementById("habitInput").value;
+    console.log(val);
+    checkCookie(val);
+    $("#toBuild").show();
+    return false;
+  })
+
+  function checkCookie(name) {
+    let cookieVal = Number(getCookie(name));
+    console.log("checkCookie()", cookieVal)
+    if (cookieVal !== "" && cookieVal !== null) {
+      console.log("cookie exists", cookieVal);
+      setCookie(name, cookieVal += 1, 30);
+    } else {
+      setCookie(name, 1, 30);
     }
   }
-  return "";
-}
-$("form").on('submit', function(event){
-  event.preventDefault();
-  let val = document.getElementById("habitInput").value;
-  console.log(val);
-  checkCookie(val);
-  return false;
-})
-function checkCookie(name) {
-  let cookieVal = Number(getCookie(name));
-  console.log("checkCookie()", cookieVal)
-  if(cookieVal !== "" && cookieVal !== null){
-    console.log("cookie exists", cookieVal);
-    setCookie(name, cookieVal += 1, 30);
-  }else{
-    setCookie(name, 1, 30);
-  }
-}
 
-function get_cookies_array() {
+  function get_cookies_array() {
 
-    var cookies = { };
+    var cookies = {};
 
     if (document.cookie && document.cookie != '') {
-        var split = document.cookie.split(';');
-        for (var i = 0; i < split.length; i++) {
-            var name_value = split[i].split("=");
-            name_value[0] = name_value[0].replace(/^ /, '');
-            cookies[decodeURIComponent(name_value[0])] = decodeURIComponent(name_value[1]);
-         }
-     }
+      var split = document.cookie.split(';');
+      for (var i = 0; i < split.length; i++) {
+        var name_value = split[i].split("=");
+        name_value[0] = name_value[0].replace(/^ /, '');
+        cookies[decodeURIComponent(name_value[0])] = decodeURIComponent(name_value[1]);
+      }
+    }
 
-     return cookies;
-}
-
-
-//Bot Page
-// $(document).ready(function() {
-//   $("#amazon").hide();
-//   $("#progress").hide();
-//
-// });
-// if (document.getElementById("amazonLink") != null) {
-//   document.getElementById("amazonLink").href += habit;
-// }
-//
-// $("#yes").click(function() {
-//   $("#yes").hide();
-//   $("#no").hide();
-//   $("#amazon").show();
-// });
-//
-// $("#no").click(function() {
-//   console.log(counter);
-//   $("#yes").hide();
-//   $("#no").hide();
-//   $("#amazon").show();
-// });
-//
-// $("#amazonLink").click(function() {
-//   console.log(counter);
-//   $("#nothx").hide();
-//   $("#progress").show();
-// });
-// $("#nothx").click(function() {
-//   console.log(counter);
-//   $("#nothx").hide();
-//   $("#progress").show();
-// });
-
-//Progress page
-var storage = get_cookies_array();
-for(var name in storage) {
-  if(document.getElementById("data") != null){
-    document.getElementById("data").innerHTML += name + " : have completed " + (storage[name]-1) + " days <br />";
+    return cookies;
   }
-}
+
+
+  //Bot Page
+  var habitToCheck = "";
+  var storage = get_cookies_array();
+  var habitsArray = [];
+  for (var names in storage) {
+    habitsArray.push(names);
+  }
+  $('#cont')
+    .append(
+      $(document.createElement('label')).prop({
+        for: 'habits'
+      }).html('Choose your habit: ')
+    )
+    .append(
+      $(document.createElement('select')).prop({
+        id: 'habits',
+        name: 'habits'
+      })
+    )
+
+  for (const val of habitsArray) {
+    $('#habits').append($(document.createElement('option')).prop({
+      value: val,
+      text: val.charAt(0).toUpperCase() + val.slice(1)
+    }))
+  };
+
+  $('#generate').click(function() {
+    var e = document.getElementById("habits");
+    habitToCheck = e.options[e.selectedIndex].text;
+    // console.log(habitToCheck)
+  });
+  habitToCheck += '';
+  if (document.getElementById("amazonLink") != null) {
+    document.getElementById("amazonLink").href += habitToCheck;
+    console.log(document.getElementById("amazonLink").href);
+    console.log(habitToCheck);
+  }
+
+  //Progress page
+  storage = get_cookies_array();
+  for (var name in storage) {
+    if (document.getElementById("data") != null) {
+      document.getElementById("data").innerHTML += name + " : have completed " + (storage[name] - 1) + " days <br />";
+    }
+  }
 
 });
